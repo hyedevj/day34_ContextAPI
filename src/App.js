7c8235ead210786dca1d5aa9b792aa61807b9fe9
hyedevj <hyedevj@gmail.com>
2023-09-08 14:29:35 +0900

    To Do를 통한 Context API 실습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import styled from "@emotion/styled";
+import Header from "./components/Header";
+import NewTaskForm from "./components/NewTaskForm";
+import TaskList from "./components/TaskList";
+import TaskProvieder from "./contexts/TaskProvider";
+
+const Container = styled.div`
+	width: 400px;
+	margin: 0 auto;
+`
+
 function App() {
-	return <div></div>
+	return (
+		<TaskProvieder>
+			<Container>
+				<Header>ToDo</Header>
+				<NewTaskForm />
+				<TaskList css={{ marginTop: 16 }} />
+			</Container>
+		</TaskProvieder>
+	)
 }
 
 export default App;
